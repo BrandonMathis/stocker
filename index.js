da6512dec7734d96b3d568c58019c6147faa8023
Brandon Mathis <bemathis@gmail.com>
2017-02-16 09:56:35 -0500

    More data

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ const rtm = new RtmClient(process.env.SLACK_TOKEN);
 const web = new WebClient(process.env.SLACK_TOKEN);
 
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-  console.log(message);
-
   if (!message.text ) { return }
   if (message.user === rtm.activeUserId) { return }
   const ticker = message.text.match(/^\$([\w]*)/)[1];
   if(ticker) {
     stocks.getquote([ticker])
       .then((json) => {
-        console.log(json);
         const img = `http://chart.finance.yahoo.com/z?s=${ticker}&z=l&t=1d&z=l`;
-        const emoji = (json[0].PercentChange.match(/^\+/))? '💵' : '⚰';
+        const positive = json[0].PercentChange.match(/^\+/);
+        const emoji = (positive)? '💵' : '⚰';
+        const color = (positive)?  '#36a64f' : '#e11d1d';
         const body = {
           as_user: true,
           attachments: [
@@ -30,21 +29,32 @@ rtm.on(RTM_EVENTS.MESSAGE, (message) => {
               pretext: json[0].Name,
               text: `${emoji} ${json[0].PercentChange} [Open ${json[0].Open}]`,
               image_url: img,
-              color: "#36a64f",
+              color: color,
               fields: [
                 {
+                  short: true,
                   title: 'Open',
-                  value: json[0].Open
+                  value: `$${json[0].Open}`
                 },
                 {
-                  title: 'Current',
-                  value: json[0].Open
+                  short: true,
+                  title: 'Last Trade',
+                  value: `$${json[0].LastTradePriceOnly}`
+                },
+                {
+                  short: true,
+                  title: 'Percent Change From Year Low',
+                  value: `${json[0].PercentChangeFromYearLow}%`
+                },
+                {
+                  short: true,
+                  title: 'Fifty Day Moving Average',
+                  value: `$${json[0].FiftydayMovingAverage}`
                 }
               ]
             }
           ]
         };
-        console.log("Posting");
         web.chat.postMessage(message.channel, '', body, (err) => {
           if (err) { console.log(err) }
         });
